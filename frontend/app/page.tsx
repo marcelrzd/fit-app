b06a853b1e35f050a2f363e6bfb3c8d47e41da2d
Marcel <mrezende@bcfsolutions.net.br>
2024-08-27 15:50:48 -0300

    feat: finishing first version of weather card

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,15 @@
-import WeatherCard from "@/components/ui/WeatherCard";
+import WeatherCard from "@/components/WeatherCard";
 
 export default function Home() {
   return (
-    <div className="h-full w-full ">
-      <div>
+    <div className="h-full w-full flex flex-col gap-8">
+      <div className="pt-8">
         <h1 className="text-4xl text-center">Good Evening Marcel!</h1>
       </div>
       <div className="container flex flex-row flex-wrap items-center justify-center w-full">
+        <WeatherCard />
+        <WeatherCard />
+        <WeatherCard />
         <WeatherCard />
       </div>
     </div>
---
+++ frontend/components/WeatherCard.tsx
@@ -0,0 +1,177 @@
+"use client";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Image from "next/image";
+import RainIcon from "@/img/266.webp";
+import Nightbg from "@/img/night-bg.jpg";
+import Daybg from "@/img/day-bg.jpg";
+
+export default function WeatherCard() {
+  const [weather, setWeather] = useState({
+    current: { last_updated: "" },
+    location: { name: "", region: "", country: "", localtime: "" },
+    forecast: {
+      forecastday: [
+        {
+          day: {
+            maxtemp_c: 0,
+            mintemp_c: 0,
+          },
+
+          hour: [
+            {
+              chance_of_rain: 0,
+              temp_c: 0,
+              condition: { text: "", icon: "" },
+              humidity: 0,
+            },
+          ],
+        },
+      ],
+    },
+  });
+  const [status, setStatus] = useState(0);
+
+  const formatTime = (time: string) => {
+    return new Date(time).toLocaleTimeString("pt-BR", {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+  };
+
+  const formatDateTime = (time: string) => {
+    return new Date(time).toLocaleString("pt-BR", {
+      hour: "2-digit",
+      minute: "2-digit",
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+    });
+  };
+
+  const formatIconUrl = (icon: string) => {
+    return icon.replace("//", "https://");
+  };
+
+  const getBackgroundStyle = () => {
+    const currentHour = new Date().getHours();
+
+    if (currentHour >= 6 && currentHour < 18) {
+      // Daytime (6 AM to 6 PM)
+      return {
+        backgroundColor: "#87CEEB", // Light Blue Sky color
+        backgroundImage: `url(${Daybg.src})`,
+        border: "1px solid #87CEEB",
+        color: "#000", // Black text
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      };
+    } else {
+      // Nighttime (6 PM to 6 AM)
+      return {
+        backgroundColor: "#1a1a2e", // Dark Starry Sky color
+        backgroundImage: `url(${Nightbg.src})`,
+        color: "#fff", // White text color
+        border: "1px solid #1a1a2e",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      };
+    }
+  };
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.weatherapi.com/v1/forecast.json?key=${
+          process.env.NEXT_PUBLIC_WEATHER_API_KEY
+        }&q=Uberlandia&aqi=no&hour=${new Date().getHours()}`
+      )
+      .then((response) => {
+        console.log(response.data);
+        setWeather(response.data);
+        setStatus(response.status);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <>
+      {status !== 200 && <p>Loading...</p>}
+      <div className="w-[100%] lg:w-[50%] p-6 ">
+        <div
+          style={getBackgroundStyle()}
+          className="border border-[#87CEEB] rounded-lg shadow-lg w-full"
+        >
+          <div className="py-3 px-6  flex justify-between">
+            <div className="flex flex-col">
+              <h1 className="text-2xl">{weather.location.name}</h1>
+              <h1 className="text-sm">
+                {formatDateTime(weather.location.localtime)}
+              </h1>
+            </div>
+            <div className="flex flex-col">
+              <h1 className="">
+                <span className="font-semibold">Min:</span>{" "}
+                {weather.forecast.forecastday[0].day.mintemp_c}°C /{" "}
+                <span className="font-semibold">Max:</span>{" "}
+                {weather.forecast.forecastday[0].day.maxtemp_c}°C
+              </h1>
+              <h1 className="text-right">
+                <span className="font-semibold">Humidity:</span>{" "}
+                {weather.forecast.forecastday[0].hour[0].humidity}%
+              </h1>
+            </div>
+          </div>
+          <div className="flex flex-row items-center justify-between px-6">
+            <div className="flex flex-row gap-2 items-center">
+              <Image
+                alt="Weather condition icon image"
+                src={formatIconUrl(
+                  weather.forecast.forecastday[0].hour[0].condition.icon
+                )}
+                width={80}
+                height={80}
+              />
+              <h1 className="text-4xl font-semibold">
+                {" "}
+                {weather.forecast.forecastday[0].hour[0].temp_c}°C
+              </h1>
+            </div>
+            <div className="flex flex-row gap-2 items-center py-3">
+              <Image
+                alt="Weather condition icon image"
+                src={RainIcon}
+                width={80}
+                height={80}
+              />
+              <div className="flex flex-col">
+                <h1 className="text-4xl font-semibold">
+                  {weather.forecast.forecastday[0].hour[0].chance_of_rain}%
+                </h1>
+                <h1 className="text-xs">precipitation</h1>
+              </div>
+            </div>
+          </div>
+
+          {/* <div className="flex justify-end w-full px-6 py-3">
+            <h1 className="text-sm">
+              Last updated: {formatTime(weather.current.last_updated)}
+              
+            </h1>
+          </div> */}
+        </div>
+      </div>
+    </>
+  );
+}
