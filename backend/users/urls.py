67282d093519486ab614ccd1a9c26af6948a6575
Marcel <mrezende@bcfsolutions.net.br>
2024-11-05 17:59:33 -0300

    change: changing user function to class based view

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,10 +1,7 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import UserViewSet
-
-router = DefaultRouter()
-router.register(r'users', UserViewSet, basename='users')
+from django.urls import path
+from .views import UserAPIView, UserDetailAPIView
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('users/', UserAPIView.as_view(), name='user-list-create'),
+    path('users/<int:pk>/', UserDetailAPIView.as_view(), name='user-detail'),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,12 +1,47 @@
-from rest_framework import viewsets, permissions
+from rest_framework.views import APIView
+from rest_framework import permissions, status
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 from .models import User
 from .serializers import UserSerializer
 
-class UserViewSet(viewsets.ModelViewSet):
-    serializer_class = UserSerializer
+class UserAPIView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
-    def get_queryset(self):
-        return User.objects.all()
+    def get(self, request):
+        # Handle GET requests to retrieve a list of users
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
 
-from django.shortcuts import render
+    def post(self, request):
+        # Handle POST requests to create a new user
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserDetailAPIView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, pk):
+        # Handle GET requests to retrieve a specific user
+        user = get_object_or_404(User, pk=pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        # Handle PUT requests to update a user
+        user = get_object_or_404(User, pk=pk)
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        # Handle DELETE requests to delete a user
+        user = get_object_or_404(User, pk=pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
