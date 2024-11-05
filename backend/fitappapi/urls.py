df42198a2eae58fa0763125aa18e6418f5b66008
Marcel <mrezende@bcfsolutions.net.br>
2024-11-05 17:53:22 -0300

    feat: creating view and serializers for users

--- backend/fitappapi/urls.py
+++ backend/fitappapi/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('body_measurements.urls')),
+    path('api/', include('users.urls')),
 
 ]
